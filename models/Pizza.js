420c45adb18919af67d00610f6f1dd684ac471ce
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-09-17 13:46:36 -0500

    final push before heroku

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,10 +3,14 @@ const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
@@ -15,6 +19,8 @@ const PizzaSchema = new Schema({
     },
     size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
     },
     toppings: [],
@@ -24,7 +30,7 @@ const PizzaSchema = new Schema({
             ref: 'Comment'
         }
     ]
-    },
+},
     {
         toJSON: {
             virtuals: true,
@@ -35,7 +41,7 @@ const PizzaSchema = new Schema({
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
+PizzaSchema.virtual('commentCount').get(function () {
     return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
